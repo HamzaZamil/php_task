e11f8c5d244335550274b9bc92c353aea231f747
HamzaZamil <hamzaalzamil@gmail.com>
2024-10-09 16:29:21 +0300

    first commit

---
+++ lesson1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+$txt = "W3schools.com";
+echo "I love $txt <br> <br>";
+ 
+
+
+$x = 4;
+$y = 3;
+
+echo $sum = $x + $y;
+
+
+?>
+
+
+
+</body>
+
+</html>
